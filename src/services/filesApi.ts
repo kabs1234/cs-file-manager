894dafbd26c6dfe0f6cc11bad5a2c4f16a61d25b
Aikhan Zhashkeyev <spy.04@mail.ru>
2025-09-17 09:56:59 +0500

    FEAT: add authorization header on every file related requests

--- src/services/filesApi.ts
+++ src/services/filesApi.ts
@@ -1,10 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL, Namespace } from '../const';
 import type { UploadFileQuery, UploadedFile } from '../types/types';
+import { getToken } from '../utils/utils';
 
 export const filesApi = createApi({
   reducerPath: Namespace.FilesAPI,
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    prepareHeaders(headers) {
+      headers.set('Authorization', `Bearer ${getToken()}`);
+
+      return headers;
+    },
+  }),
   endpoints: (build) => ({
     uploadFile: build.mutation<UploadedFile, UploadFileQuery>({
       query: (user) => {
